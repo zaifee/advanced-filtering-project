d4ed176dec724233884d213a6caa7c930a8c8185
Mohd Nawaz <nawazthezaifre@gmail.com>
2025-07-23 21:11:02 +0530

    logic

--- src/Category/Category.jsx
+++ src/Category/Category.jsx
@@ -1,12 +1,13 @@
+import Input from '../components/Input';
 import './Category.css';
 
-const Category = () => {
+const Category = ({handleChange}) => {
   return (
    <>
     <div className="sidebar-title">
       <h3>Category</h3>
 
-      <div className='sidebar-items'>
+      {/* <div className='sidebar-items'>
         <label className="sidebar-label-container">
           <input type="radio" name='test'/>
           <span className="checkmark"></span> All
@@ -27,6 +28,35 @@ const Category = () => {
           <input type="radio" name='test'/>
           <span className="checkmark"></span> Heals
         </label>
+      </div> */}
+      <div>
+       <label className="sidebar-label-container">
+        <input onChange={handleChange} type="radio" value="" name="test"/>
+        <span className="checkmark"></span>All
+       </label>
+       <Input 
+       handleChange={handleChange}
+       value="sneakers"
+       title="Sneakers"
+       name="test"
+       
+       />
+
+      <Input 
+       handleChange={handleChange}
+       value="Sandals"
+       title="Sandals"
+       name="test1"
+       
+       />
+
+      <Input 
+       handleChange={handleChange}
+       value="heels"
+       title="Heels"
+       name="test2"
+       
+       />
       </div>
 
     </div>
